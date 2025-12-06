2d6b77f659f466b413bf5077c359f72a5960e6c4
swathihosagpudr <swathihosagoudr@gamil.com>
2025-12-06 12:18:21 +0530

    save test file

---
+++ test_employee.py
@@ -0,0 +1,12 @@
+
+from employee import employee_details
+
+def test_employee_details():
+    expected_output = (
+        "Employee Name: Alice\n"
+        "Employee ID: E1001\n"
+        "Department: IT\n"
+        "Salary: 55000"
+    )
+
+    assert employee_details("swathi", "E304", "IT", 55000) == expected_output
